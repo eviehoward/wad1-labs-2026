dbfcb24407f0c622061617fa0a8440e6364bdf12
Evie Orbit <evie.howard2305@gmail.com>
2026-04-17 13:34:26 +0100

    Search and sort functions completed

--- routes.js
+++ routes.js
@@ -19,6 +19,9 @@ router.get("/playlist/:id", playlist.createView);
 router.get("/error", (request, response) => response.status(404).end("Page not found."));
 router.get("/stats", stats.createView);
 
+router.get('/searchCategory', dashboard.createView);
+router.get('/sortData', dashboard.createView);
+
 router.post("/playlist/:id/addsong", playlist.addSong);
 router.post("/dashboard/addplaylist", dashboard.addPlaylist);
 router.post("/playlist/:id/updatesong/:songid", playlist.updateSong);
