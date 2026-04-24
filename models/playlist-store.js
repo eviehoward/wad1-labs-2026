b53848932cf78c3c052aff245b44e7dd328ae24f
Evie Orbit <evie.howard2305@gmail.com>
2026-04-24 13:50:01 +0100

    Image upload functionality added

--- models/playlist-store.js
+++ models/playlist-store.js
@@ -25,14 +25,34 @@ const playlistStore = {
 },
 
 
-  addPlaylist(playlist) {
-    this.store.addCollection(this.collection, playlist);
+    async addPlaylist(playlist, file, response) {
+    try {
+      playlist.picture = await this.store.addToCloudinary(file);
+      this.store.addCollection(this.collection, playlist);
+      response();
+    } catch (error) {
+      logger.error("Error processing playlist:", error);
+      response(error);
+    }
   },
 
-  removePlaylist(id) {
+
+    async removePlaylist(id, response) {
     const playlist = this.getPlaylist(id);
+
+    if (playlist.picture && playlist.picture.public_id) {
+      try {
+        await this.store.deleteFromCloudinary(playlist.picture.public_id);
+        logger.info("Cloudinary image deleted");
+      } catch (err) {
+        logger.error("Failed to delete Cloudinary image:", err);
+      }
+    }
+
     this.store.removeCollection(this.collection, playlist);
-},
+    response();
+  },
+
 
   editSong(id, songId, updatedSong) {
     this.store.editItem(this.collection, id, songId, this.array, updatedSong);
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import logger from "./utils/logger.js";
 import { create } from "express-handlebars";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
 
 const app = express();
@@ -16,6 +17,8 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cookieParser());
+
+app.use(fileUpload({useTempFiles: true}));
 
 const handlebars = create({
   extname: ".hbs",
